bbb41562bd06e53e6119b2938b8779b3b10fa829
sohrab salehi <sohrab@sohrabs-MacBook-Air.local>
2023-05-29 19:31:20 +0200

    Add responsive view for navbar

--- src/MainLayout/index.tsx
+++ src/MainLayout/index.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { Layout, Menu } from "antd";
+import { Drawer, Layout, Menu } from "antd";
+import {
+    MenuOutlined,
+    LoginOutlined,
+    UserAddOutlined,
+} from "@ant-design/icons";
+
+import "./MainLayout.scss";
+import digitLogo from "../assets/logo.png";
 
 const { Header, Content, Footer } = Layout;
 
 function MainLayout(): JSX.Element {
+    const [mobileMenu, setMobileMenu] = useState(false);
     const location = useLocation();
     const selectedNavbarItem = () => {
         switch (location.pathname) {
@@ -22,25 +31,56 @@ function MainLayout(): JSX.Element {
     return (
         <Layout style={{ minHeight: "100vh" }}>
             <Header className="header">
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    selectedKeys={[selectedNavbarItem()]}
-                    items={[
-                        {
-                            key: "home",
-                            label: <Link to="/">Home</Link>,
-                        },
-                        {
-                            key: "login",
-                            label: <Link to="/login">Login</Link>,
-                        },
-                        {
-                            key: "register",
-                            label: <Link to="/register">Register</Link>,
-                        },
-                    ]}
-                />
+                <div className="logo">
+                    <Link to="/">
+                        <img src={digitLogo} alt="logo" />
+                    </Link>
+                </div>
+                <span className="main-nav">
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        selectedKeys={[selectedNavbarItem()]}
+                        items={[
+                            {
+                                key: "login",
+                                label: <Link to="/login">Login</Link>,
+                            },
+                            {
+                                key: "register",
+                                label: <Link to="/register">Register</Link>,
+                            },
+                        ]}
+                    />
+                </span>
+
+                <div className="mobile-menu-button">
+                    <MenuOutlined onClick={() => setMobileMenu(true)} />
+                </div>
+                <Drawer
+                    open={mobileMenu}
+                    onClose={() => setMobileMenu(false)}
+                    style={{ backgroundColor: "#001628" }}
+                    closable={false}
+                >
+                    <Menu
+                        theme="dark"
+                        mode="vertical"
+                        selectedKeys={[selectedNavbarItem()]}
+                        items={[
+                            {
+                                key: "login",
+                                label: <Link to="/login">Login</Link>,
+                                icon: <LoginOutlined />,
+                            },
+                            {
+                                key: "register",
+                                label: <Link to="/register">Register</Link>,
+                                icon: <UserAddOutlined />,
+                            },
+                        ]}
+                    />
+                </Drawer>
             </Header>
             <Content style={{ padding: "50px" }}>
                 <Outlet />
