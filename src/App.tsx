4da44edf5a64f23fa7e3291991d42a0792d0e064
sohrab salehi <sohrab@sohrabs-MacBook-Air.local>
2023-05-29 14:44:14 +0200

    Add ant design and configure router with Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import MainLayout from "./MainLayout";
+// import Login from "./Login";
+import ErrorPage from "./ErrorPage";
+
 import "./App.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        // element: <MainLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            // {
+            //     element: <Login />,
+            //     index: true,
+            // },
+        ],
+    },
+]);
+
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
